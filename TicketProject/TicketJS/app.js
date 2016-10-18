c3556b5d915f0756bf61dd8ce7a5b63e4b5c6fe5
RATTANAPORN SOMNUK <5630213004@email.psu.ac.th>
2016-10-18 17:57:26 +0700

    update

--- TicketProject/TicketJS/app.js
+++ TicketProject/TicketJS/app.js
@@ -1,17 +1,14 @@
-﻿var app = angular.module('Ticket', ['ngRoute', 'AxelSoft', 'ui.router']);
+﻿/// <reference path="Ticket/OpenTicket.js" />
+/// <reference path="Ticket/OpenTicket.js" />
+var app = angular.module('Ticket', ['AxelSoft', 'oc.lazyLoad', 'ui.router']);
 
-app.config(function ($stateProvider) {
+app.config(function ($stateProvider, $urlRouterProvider, $ocLazyLoadProvider) {
 
-        .when("/", {
-            templateUrl: "Home/Home"
-        })
-          .when("/AllTicket", {
-              templateUrl: "Ticket/AllTicket"
-          })
-         .when("/OpenTicket", {
-             templateUrl: "Ticket/OpenTicket"
-         })
-        .otherwise({
-            templateUrl: "/"
-        })
-});
+    $urlRouterProvider.otherwise("/")
+    $stateProvider.state('/eee', {
+        url: "/sss",
+        templateUrl: '/Ticket/AllTicket',
+        controller: 'OpenTicketController',
+       
+    })
+})
