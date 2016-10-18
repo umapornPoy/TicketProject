e3661bba7c49c4cf232f627f516d1a4541e0ccb6
umapornPoy <p.tunlang7@gmail.com>
2016-10-18 11:48:16 +0700

    1. initial project 2. index update

---
+++ TicketProject/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TicketProject.Global" Language="C#" %>
---
+++ TicketProject/TicketJS/app.js
@@ -0,0 +1,12 @@
+﻿var app = angular.module('Ticket', ['ngRoute', 'ui.bootstrap']);
+app.config(function ($routeProvider) {
+    $routeProvider
+
+        .when("/", {
+            templateUrl: "Home/Index"
+        })
+         
+        .otherwise({
+            templateUrl: "/"
+        })
+});
